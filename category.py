45e5d8b5cc6f67b282971e6d865932b2a2e81963
JihyoKim <44666091+ksh04023@users.noreply.github.com>
2020-07-22 22:49:22 +0900

    categorizing

--- category.py
+++ category.py
@@ -2,13 +2,13 @@ import json
 import copy
 from openpyxl import Workbook
 from collections import OrderedDict
+import pprint
 
-data_json_path = 'C:\\Users\\sejin\\Documents\\GitHub\\DataManufacture\\datacollect.json'
-categories_json_path = 'C:\\Users\\sejin\\Documents\\GitHub\\DataManufacture\\categories.json'
-
+data_json_path = 'C:\\Users\\ksh04\\PythonProjects\\DataManufacture\\datacollect.json'
+categories_json_path = 'C:\\Users\\ksh04\\PythonProjects\\DataManufacture\\categories.json'
 # CELL_DATE = 1
 CELL_LAST_USED = 6
-CELL_PACKAGE_NAME = 5 
+CELL_PACKAGE_NAME = 5
 CELL_TOTAL_TIME_IN_FOREGROUND = 4
 CELL_TIMESTAMPS = 2
 CELL_USER_NAME = 1
@@ -53,60 +53,37 @@ with open(data_json_path, encoding= 'UTF-8') as json_file:
     usersData = json.load(json_file)
     users = usersData['user']
 
-    wb = Workbook()
-    sheet1 = wb.active
-    sheet1.title = 'untitled'
-    idx = 1
+
+    idx = 0
     jsonData = dict()
     #appending each user data
     for user_name in users:
         jsonData[user_name] = {}
         for property in users[user_name]:
-            index = 0
+            
             if property == 'usagestatsCoroutine':
                 for coroutines in users[user_name][property]:
-<<<<<<< HEAD
-                    
-=======
-                    # if idx>= MAX_CATEGORIES_BY_COROUTINE:
-                    #     break
->>>>>>> bf13dadfa5334b93f6d4e5c2bc1edbbad207e33b
-                    sheet1.cell(idx, CELL_USER_NAME).value = user_name
-                    jsonData[user_name]['userName'] = user_name
-                    # print(type(jsonData[user_name]))
+                    jsonData[user_name][coroutines] = {}
+                    idx = 0
                     for coroutine_attr in users[user_name][property][coroutines]:
                         # if coroutine_attr == 'date':
                         #     sheet1.cell(idx, CELL_DATE).value = users[user_name][property][coroutines][coroutine_attr]
                         
                         
                         if coroutine_attr == 'timestamp':
-<<<<<<< HEAD
-                            sheet1.cell(idx, CELL_TIMESTAMPS).value = int((int(users[user_name][property][coroutines][coroutine_attr])/1000))
-                            jsonData[user_name]['timestamp'] = int((int(users[user_name][property][coroutines][coroutine_attr])/1000))
-
-=======
-                            sheet1.cell(idx, CELL_TIMESTAMPS).value = str(int(users[user_name][property][coroutines][coroutine_attr]))
-                            tempTime = str(int(users[user_name][property][coroutines][coroutine_attr]))
-                            jsonData[user_name]['timestamp'] = tempTime
->>>>>>> bf13dadfa5334b93f6d4e5c2bc1edbbad207e33b
-                        if coroutine_attr == 'statsList':
+                            jsonData[user_name][coroutines]['timestamp'] = int((int(users[user_name][property][coroutines][coroutine_attr])/1000))
+                        if coroutine_attr == 'statsList': # each app
                             start_idx = idx
-                            
                             for element in users[user_name][property][coroutines][coroutine_attr]:
-                                
                                 #case1 코루틴의 statsList 개수가 5개가 이상일 경우
                                 uncategorizable = False
-                                for stat_attr in element:
+                                for stat_attr in element: 
                                     # if stat_attr == 'lastTimeUsed':
                                     #     sheet1.cell(idx, CELL_LAST_USED).value = element[stat_attr]
+                                    
                                     if stat_attr == 'packageName':
-<<<<<<< HEAD
-                                        jsonData[user_name][str(index)]={}
-=======
-                                        jsonData[user_name][index] = {}
->>>>>>> bf13dadfa5334b93f6d4e5c2bc1edbbad207e33b
-                                        sheet1.cell(idx, CELL_PACKAGE_NAME).value = element[stat_attr]
-                                        jsonData[user_name][str(index)]['packageName'] =  element[stat_attr]
+                                        jsonData[user_name][coroutines][str(idx)]={}
+                                        # jsonData[user_name][coroutines][str(index)]['packageName'] =  element[stat_attr]
                                         if element[stat_attr] in categories_dict.keys():
                                             #Integer로 Mapping한 카테고리 dictionary 내에 값이 있는지 확인.
                                             category_value = categories_dict[element[stat_attr]]
@@ -139,54 +116,69 @@ with open(data_json_path, encoding= 'UTF-8') as json_file:
                                                 category_label = 13
                                             elif category_value == BROWSER_STRING:
                                                 category_label = 14
-                                            sheet1.cell(idx, CELL_CATEGORY).value = category_label
-                                            jsonData[user_name][str(index)]['category'] =  category_label
+                                            
+                                            jsonData[user_name][coroutines][str(idx)]['category'] =  category_label
 
                                         else:
                                             #Play store에 없는 APP의 경우.
                                             uncategorizable = True
                                             non_categorizable.add(element[stat_attr])
+                                            continue
+
                                         
                                     if stat_attr == 'totalTimeInForeground':
-                                        sheet1.cell(idx, CELL_TOTAL_TIME_IN_FOREGROUND).value = element[stat_attr]
-                                        jsonData[user_name][index]['totalTimeInForeground'] = element[stat_attr]
                                         if not uncategorizable:
-                                            idx = idx + 1
-
-<<<<<<< HEAD
-                            index = index + 1
-                            if idx >= MAX_CATEGORIES_BY_COROUTINE:
-                                break
-=======
-                                index = index + 1
->>>>>>> bf13dadfa5334b93f6d4e5c2bc1edbbad207e33b
-                            
+                                            jsonData[user_name][coroutines][str(idx)]['totalTimeInForeground'] = element[stat_attr]
+                                            idx += 1
+                                    if idx >= MAX_CATEGORIES_BY_COROUTINE:
+                                        break
+                                    
+                                    
             
-                            
                             #case2 코루틴의 statsList 개수가 5개 미만일 경우
-                            if idx - start_idx < MAX_CATEGORIES_BY_COROUTINE:
-                                for i in range(0, MAX_CATEGORIES_BY_COROUTINE - (idx - start_idx)):
-                                    sheet1.cell(idx, CELL_LAST_USED).value = PADDING_STATE
-                                    sheet1.cell(idx, CELL_PACKAGE_NAME).value = PADDING_STATE
-                                    sheet1.cell(idx, CELL_CATEGORY).value = PADDING_STATE
-                                    sheet1.cell(idx, CELL_TOTAL_TIME_IN_FOREGROUND).value = PADDING_STATE
-<<<<<<< HEAD
-                                    jsonData[user_name][str(index)] = 0
-=======
-
->>>>>>> bf13dadfa5334b93f6d4e5c2bc1edbbad207e33b
+                            if idx < MAX_CATEGORIES_BY_COROUTINE:
+                                for i in range(MAX_CATEGORIES_BY_COROUTINE - idx):
+                                    jsonData[user_name][coroutines][str(idx)] = 0
                                     idx += 1
-    print(jsonData)
-               
+                                    if idx >= MAX_CATEGORIES_BY_COROUTINE: break
+
+                            if idx == 0:#statslist가 없으면
+                                for i in range(5):
+                                    jsonData[user_name][coroutines][str(i)] = 0
+                                    
+
+    pp = pprint.PrettyPrinter(width=41, compact=True)
+    # pp.pprint(jsonData)
 
 
-    wb.save(filename= 'C:\\Users\\sejin\\Documents\\GitHub\\DataManufacture\\data.xlsx')
-    # print(jsonData)
+    for user in jsonData:
+        for eachCoroutine in jsonData[user]:
+            print(user)
+            print(eachCoroutine)
+            if '0' and '4' in jsonData[user][eachCoroutine]:
+                print(jsonData[user][eachCoroutine]['0'])
+                print(jsonData[user][eachCoroutine]['4'])
+                jsonData[user][eachCoroutine]['0'],jsonData[user][eachCoroutine]['4'] = jsonData[user][eachCoroutine]['4'],jsonData[user][eachCoroutine]['0']
+                jsonData[user][eachCoroutine]['1'],jsonData[user][eachCoroutine]['3'] = jsonData[user][eachCoroutine]['3'],jsonData[user][eachCoroutine]['1']
+                print(jsonData[user][eachCoroutine]['4'])
+                print(jsonData[user][eachCoroutine]['0'])
+                # for eachApp in eachCoroutine:
+
+                #     if eachApp == '0':
+
+                #         category_temp = eachApp['category']
+                #         total_temp = eachApp['totalTimeInForeground']
+                #         jsonData[user][eachApp]['category'] = jsonData[user]['4']['category']
+                #         jsonData[user][eachApp]['totalTimeInForeground'] = jsonData[user]['4']['totalTimeInForeground']
+                #         jsonData[user]['4']['category'] = category_temp
+                #         jsonData[user][['4']]['totalTimeInForeground'] = total_temp
+
     
-    file_path = 'C:\\Users\\sejin\\Documents\\GitHub\\DataManufacture\\before_puls.json'
+    file_path = "C:\\Users\\ksh04\\PythonProjects\\DataManufacture\\appstats.json"
     with open(file_path, 'w') as outfile:
         json.dump(jsonData, outfile)
 
-
-
 # print(categories_dict)
+
+
+
