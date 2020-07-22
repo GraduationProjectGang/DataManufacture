5ade3e71ca6836b445e28805722fef3710dc47bd
JihyoKim <44666091+ksh04023@users.noreply.github.com>
2020-07-22 18:23:26 +0900

    dd

--- dataTraining.py
+++ dataTraining.py
@@ -13,20 +13,31 @@ jsonPath = 'C:\\Users\\ksh04\\PythonProjects\\DataManufacture\\data.json'
 
 with open(jsonPath, encoding= 'UTF-8') as json_file:
        data = json.load(json_file)
-       userDataArray=[]
+       dataArray = [] #all users
        for user in data:
+              userArray=[] # 5 coArray Maximum
               for coroutine in data[user]:
-                     # print(coroutine)
-                     userData = []
+                     coArray = [] #coroutineArray
                      for item in coroutine:
-                            userData.append(coroutine[item])
-                     userDataArray.append(userData)
-                     # print(userDataArray)
+                            coArray.append(coroutine[item])
+                     userArray.append(coArray)
+              dataArray.append(userArray)
+
+
+       print(len(dataArray))
+              
                      
 
+       # #print(type(dataArr))
+       # print(dataArr[dataArr.shape[0]-1])
+       # print(dataArr.shape)
 
-       dataArr = np.array(userDataArray)
-       #print(type(dataArr))
-       print(dataArr[dataArr.shape[0]-1])
-       print(dataArr.shape)    
+       # with open('realData.json', 'w') as outfile:
+       #          json.dump(data, outfile)
+       
+       # print(type(dataArr))
+
+
+
+ 
        
